084a82ba88ddaedbacf7c4e409d4dbbc35887c85
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-19 14:26:59 +0100

    feat:Added UserViewSet and logoutview to the urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='users')
 
 urlpatterns = [
+    path('', include(router.urls)),
+
     path('auth/register/', views.UserRegistrationView.as_view(), name='register'),
     path('auth/login/', views.UserLoginView.as_view(), name='login'),
-    path('verify-email/', views.VerifyEmailView.as_view(), name='verify-email'),
+    path('auth/verify-email/', views.VerifyEmailView.as_view(), name='verify-email'),
+    path('auth/logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
+
+    
 ]
