fe75c22340418412499c0977a9e2b10493b13635
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-19 11:24:33 +0100

    feat: update auth backend to support new authentication workflow

--- backend/apps/users/backends.py
+++ backend/apps/users/backends.py
@@ -5,13 +5,13 @@ from django.contrib.auth.backends import ModelBackend
 User = get_user_model()
 class EmailTokenAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        email = username
         try:
-            user = User.objects.get(email=username)
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
         except User.DoesNotExist:
             return None
-
-        if user.check_password(password):
-            return user
         return None
 
  
