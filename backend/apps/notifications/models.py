b87365cf128556bb7c77a4243b3a4b634348d374
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-21 20:32:09 +0100

    refactor: update notifications model structure and fields

--- backend/apps/notifications/models.py
+++ backend/apps/notifications/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import uuid
-
+from django.conf import settings
 class Notification(models.Model):
     notification_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     NOTIFICATION_TYPES = (
@@ -13,10 +13,9 @@ class Notification(models.Model):
         ('profile_view', 'Profile View'),
     )
     
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='notifications')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
     notification_type = models.CharField(max_length=50, choices=NOTIFICATION_TYPES)
     actor = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='acted_notifications')
-    target_object_id = models.PositiveIntegerField()  
     target_content_type = models.CharField(max_length=100)  
     message = models.TextField()
     is_read = models.BooleanField(default=False)
