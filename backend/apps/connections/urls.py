a5a15bf557d6ef688d3df7465796048e384dd2d0
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-21 20:36:29 +0100

    feat: implement connections URL routing

---
+++ backend/apps/connections/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ConnectionViewSet, FollowViewSet
+
+router = DefaultRouter()
+
+router.register(r'connections', ConnectionViewSet, basename='connections')
+router.register(r'follows', FollowViewSet, basename='follows')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
