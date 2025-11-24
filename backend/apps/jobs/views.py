14f7f2389ab6e371b05498ce8bab5a26bd0c3008
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-24 20:12:27 +0100

    fix: sync code formatting

--- backend/apps/jobs/views.py
+++ backend/apps/jobs/views.py
@@ -7,10 +7,13 @@ from rest_framework.response import Response
 
 from .models import Job, JobApplication
 from .search import JobSearch
-from .serializers import (JobApplicationApplySerializer,
-                          JobApplicationCreateSerializer,
-                          JobApplicationSerializer, JobCreateSerializer,
-                          JobSerializer)
+from .serializers import (
+    JobApplicationApplySerializer,
+    JobApplicationCreateSerializer,
+    JobApplicationSerializer,
+    JobCreateSerializer,
+    JobSerializer,
+)
 
 
 class JobViewSet(viewsets.ModelViewSet):
