978e51301180f47248e570cde9572292e6645479
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-24 20:08:34 +0100

    fix: force isort import sorting

--- backend/apps/companies/views.py
+++ backend/apps/companies/views.py
@@ -6,12 +6,8 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Company
-from .serializers import (
-    CompanyCreateSerializer,
-    CompanySerializer,
-    CompanyStatsSerializer,
-    CompanyUpdateSerializer,
-)
+from .serializers import (CompanyCreateSerializer, CompanySerializer,
+                          CompanyStatsSerializer, CompanyUpdateSerializer)
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
