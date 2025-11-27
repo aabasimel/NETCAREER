25c98f2c006b7381dc7042d1f0491c78a491338e
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-27 14:54:31 +0100

    feat: implement background task processing with updated settings

--- backend/apps/users/tasks.py
+++ backend/apps/users/tasks.py
@@ -3,24 +3,24 @@ import logging
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+import resend
 
 logger = logging.Logger(__name__)
 
 resend.api_key = settings.RESEND_API_KEY
 
+
 @shared_task
 def send_email_verification(email, link):
     try:
         params = {
-        "from": "NetCareer <onboarding@resend.dev>",
-        "to": [email],
-        "subject": "Email verification",
-        "html": f'<p>Click to verify: <a href="{link}">{link}</a></p>'
+            "from": "NetCareer <onboarding@resend.dev>",
+            "to": [email],
+            "subject": "Email verification",
+            "html": f'<p>Click to verify: <a href="{link}">{link}</a></p>',
         }
-        
+
         resend.Emails.send(params)
-
-
 
         return f"Verification email sent to {email}"
     except Exception as e:
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -215,7 +215,7 @@ REST_FRAMEWORK = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
